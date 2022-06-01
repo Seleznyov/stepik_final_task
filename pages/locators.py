4477b7253715fd1a70d4a5e31dfa40d135057556
stas <58526725+Seleznyov@users.noreply.github.com>
2022-06-01 20:45:41 +0300

    4.3.1
    рабочая версия

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,19 @@
 from selenium.webdriver.common.by import By
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     Registr_Form = (By.CSS_SELECTOR, "#register_form")
     Login_Form = (By.CSS_SELECTOR, "#login_form")
+
+class BasketLocators():
+    Basket = (By.CSS_SELECTOR, "[class='btn btn-lg btn-primary btn-add-to-basket']")
+
+class OrderLocators():
+    Order = (By.CSS_SELECTOR, "[class='alertinner ']")
+    Name_Order = (By.XPATH, "//div[@class='col-sm-6 product_main']/h1")
+
+class PricerLocators():
+    Price_Basket = (By.XPATH, "//div[@class='alertinner ']/p/strong")
+    Price_Order = (By.XPATH, "//div[@class='col-sm-6 product_main']/p[@class='price_color']")
---
+++ pages/product_page.py
@@ -0,0 +1,42 @@
+from .base_page import BasePage
+from .locators import BasketLocators
+from .locators import OrderLocators
+from .locators import PricerLocators
+
+import math
+
+class ProductPage(BasePage):
+    def add_basket(self):
+        Basket = self.browser.find_element(*BasketLocators.Basket)
+        Basket.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def should_item_added_to_cart(self):
+        order = self.browser.find_element(*OrderLocators.Order)
+        test_result = order.text
+        name_order = self.browser.find_element(*OrderLocators.Name_Order)
+        test_name = name_order.text
+        print(test_name)
+        assert test_name in test_result, "имя заказа не совпадает"
+
+    def should_price(self):
+        basket = self.browser.find_element(*PricerLocators.Price_Basket)
+        Price_Basket = basket.text
+        order = self.browser.find_element(*PricerLocators.Price_Order)
+        Price_Order = order.text
+        print(Price_Basket)
+        assert Price_Basket == Price_Order, "Стоимость разная"
+
